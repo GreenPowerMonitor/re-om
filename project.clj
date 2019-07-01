b181bfcc1ff3640600b83aca18246fbc5ccc35d7
Joel Sánchez <webmaster@kazer.es>
2019-07-01 12:43:50 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject greenpowermonitor/re-om "0.1.0-SNAPSHOT"
+(defproject greenpowermonitor/re-om "0.1.0"
   :description "A re-frame inspired Om framework for both writing new SPA and giving new life to existing legacy SPAs"
 
   :url "https://github.com/GreenPowerMonitor/re-om"
